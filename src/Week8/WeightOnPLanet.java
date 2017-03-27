1af905eee375f3a9556b5f6c8484680f5e080619
Gabriel Ulrich <ulrichgw06@mounties.mansfield.edu>
2017-03-27 07:36:01 -0500

    CIS3315JavaFX - update

--- src/Week8/WeightOnPLanet.java
+++ src/Week8/WeightOnPLanet.java
@@ -1,8 +1,6 @@
 package Week8;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,7 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -42,7 +39,7 @@ public class WeightOnPLanet extends Application {
         GridPane gridPane = new GridPane();
         gridPane.setStyle("-fx-hgap: 10; -fx-vgap: 10; -fx-padding: 20; -fx-alignment: center;");
 //------------------------------------------------------------------------------
-        Label lbEarth = new Label("Weight on EARTH -->");
+        Label lbEarth = new Label("Weight in Pounds on EARTH -->");
         lbEarth.setStyle("-fx-font-size: 10pt; -fx-text-fill: #0000ff;");
         gridPane.add(lbEarth, 0, 0);
 
@@ -56,67 +53,73 @@ public class WeightOnPLanet extends Application {
 
         tfMoon = new TextField();
         tfMoon.setEditable(false);
+        tfMoon.setPromptText("In Pounds... ");
         gridPane.add(tfMoon, 1, 1);
 //------------------------------------------------------------------------------
         Label lbVenus = new Label("VENUS -->");
         lbVenus.setStyle("-fx-font-size: 10pt; -fx-text-fill: #0000ff;");
         gridPane.add(lbVenus, 0, 2);
-        
+
         tfVenus = new TextField();
         tfVenus.setEditable(false);
+        tfVenus.setPromptText("In Pounds... ");
         gridPane.add(tfVenus, 1, 2);
 //------------------------------------------------------------------------------
         Label lbMars = new Label("MARS -->");
         lbMars.setStyle("-fx-font-size: 10pt; -fx-text-fill: #0000ff;");
         gridPane.add(lbMars, 0, 3);
-        
+
         tfMars = new TextField();
         tfMars.setEditable(false);
+        tfMars.setPromptText("In Pounds... ");
         gridPane.add(tfMars, 1, 3);
 //------------------------------------------------------------------------------
         Label lbJupiter = new Label("JUPITER -->");
         lbJupiter.setStyle("-fx-font-size: 10pt; -fx-text-fill: #0000ff;");
         gridPane.add(lbJupiter, 0, 4);
-        
+
         tfJupiter = new TextField();
         tfJupiter.setEditable(false);
+        tfJupiter.setPromptText("In Pounds... ");
         gridPane.add(tfJupiter, 1, 4);
 //------------------------------------------------------------------------------
 
         Button btn = new Button();
-        btn.setText("Calculate weights...");
-        btn.setStyle("-fx-font-size: 10pt; -fx-padding: 10; -fx-text-fill: #0000ff;");
-//        btn.setOnAction(e -> calculate());
+        btn.setText("Calculate Weights...");
+        btn.setStyle("-fx-font-size: 18pt; -fx-padding: 10; -fx-text-fill: #0000ff;");
+        btn.setOnAction(e -> calculate());
 
-        HBox hboxBtn = new HBox();
-        hboxBtn.setAlignment(Pos.CENTER);
-        hboxBtn.getChildren().add(btn);
+        HBox hBoxBtn = new HBox();
+        hBoxBtn.setAlignment(Pos.CENTER);
+        hBoxBtn.getChildren().add(btn);
 
-        BorderPane borderPane = new BorderPane();
-        borderPane.setStyle("-fx-padding: 20;");
-        borderPane.setTop(hboxTitle);
-        borderPane.setCenter(gridPane);
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-padding: 20");
+        root.setTop(hboxTitle);
+        root.setCenter(gridPane);
+        root.setBottom(hBoxBtn);
 
-        Scene scene = new Scene(borderPane, 400, 600);
+        Scene scene = new Scene(root, 400, 400);
 
-        primaryStage.setTitle("Weights on Planets");
+        primaryStage.setTitle("Weights!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public void calculate() {
-        double weight = Double.parseDouble(tfEarth.getText());
-        double moonWeight = weight + GRAVITY_MOON;
-        tfMoon.setText(String.format("%.2f" , moonWeight));
+        double EarthWeight = Double.parseDouble(tfEarth.getText());
         
-        double venusWeight = weight + GRAVITY_VENUS;
-        tfVenus.setText(String.format("%.2f" , venusWeight));
-        
-        double marsWeight = weight + GRAVITY_MARS;
-        tfMars.setText(String.format("%.2f" , marsWeight));
-        
-        double jupWeight = weight + GRAVITY_JUPITER;
-        tfJupiter.setText(String.format("%.2f" , jupWeight));
+        double moonWeight = EarthWeight * GRAVITY_MOON;
+        tfMoon.setText(String.format("%.2f", moonWeight));
+
+        double venusWeight = EarthWeight * GRAVITY_VENUS;
+        tfVenus.setText(String.format("%.2f", venusWeight));
+
+        double marsWeight = EarthWeight * GRAVITY_MARS;
+        tfMars.setText(String.format("%.2f", marsWeight));
+
+        double jupWeight = EarthWeight * GRAVITY_JUPITER;
+        tfJupiter.setText(String.format("%.2f", jupWeight));
     }
 
     /**
